66743a6b75978ee7e50018fb56f2492cb362dcb2
albertommj <albertommj@gmail.com>
2019-11-04 22:40:08 +0100

    Delete ej1.py

--- Python3/Secuenciales/ej1/ej1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
-#1.preguntar nombre
-#2.almacenarlo en una variable
-#3.imprimir por pantalla: hola nombre
-
-nombre=input("¿Cuál es tu nombre?> ")
-print("Hola "+nombre)
