8f1ea97a2dbcf2eecbac8a0cb54ff30fa2bbcb86
albertommj <albertommj@gmail.com>
2019-11-14 10:04:52 +0100

    Update ej8.py

--- Python3/Repetitivas/ej8.py
+++ Python3/Repetitivas/ej8.py
@@ -10,9 +10,21 @@ print("\/\/\/CRONOMETRO\/\/\/")
 contador = 0
 while True:
     #print(contador)
-    tiempo = time.gmtime(time.time())
-    print(tiempo.tm_hour,":",tiempo.tm_min,":",tiempo.tm_sec)
+    #tiempo = time.gmtime(time.time())
+    #print(tiempo.tm_hour,":",tiempo.tm_min,":",tiempo.tm_sec)
+    print(horas,":",minutos,".",segundos)
     time.sleep(1)
+    if  segundos < 59:
+	segundos += 1
+    else:
+	segundo = 0
+	minutos += 1
+	
+    if  minutos < 59:
+	minutos += 1
+    else:
+	minutos = 0
+	horas += 1
     contador = contador + 1
     if(os.name=="posix"): #Si el SO es linux o mac
         os.system("clear")
