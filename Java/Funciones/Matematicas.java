d2488ed881231d00da4db57a827b604b4cff2d7d
albertommj <albertommj@gmail.com>
2019-12-04 16:24:36 +0100

    OJO no terminado ni testeo completado

--- Java/Funciones/Matematicas.java
+++ Java/Funciones/Matematicas.java
@@ -2,7 +2,7 @@ package matematicas;
 
 public class Matematicas {
 	
-//******Funciones Básicas******
+
 /**
  * devuelve si el entero pasado por parámetro
  * es primo o no	
@@ -53,9 +53,9 @@ public class Matematicas {
  * @return numeroC int
  */
 	public static int juntaNumeros(int numeroA, int numeroB) {
-		return Integer.valueOf(String.valueOf(numeroA) + String.valueOf(numeroB));
+		return pegarPorDetras(numeroA,numeroB);
 	}
-//******Funciones Complejas******
+
 /**
  * devuelve el número pasado por parámetro
  * en orden inverso
@@ -64,9 +64,10 @@ public class Matematicas {
  */
 	public static int voltea(int numero) {
 		int invertido = 0;
-		for(int i=0;i<digitos(numero);i++) {
-			invertido = invertido*10+(numero%10);
-			numero/=10;
+		int longitud = digitos(numero);
+		for(int i=0;i<longitud;i++) {
+			invertido = Matematicas.juntaNumeros(invertido, numero%10);
+			numero = numero/10;
 		}
 		return invertido;
 	}
@@ -88,8 +89,8 @@ public class Matematicas {
  * @param numero int
  * @return boolean
  */
-	public static boolean esCapicua(int numero) {
-		if(voltea(numero)==numero) {
+	public static boolean esCapicua(int numero) { 
+		if(numero==voltea(numero)) {
 			return true;
 		}else{
 			return false;
@@ -101,7 +102,7 @@ public class Matematicas {
  * @return int
  */
 	public static int digitoN(int numero,int n) {
-		return Integer.valueOf(String.valueOf(numero).charAt(n));//obtiene la cifra como caracter de una cadena y lo devuelve como entero	
+		return trozoDeNumero(numero,n,n+1);	
 	}
 /**
  * devuelve la posición de la primera ocurrencia de un dígito
@@ -115,9 +116,76 @@ public class Matematicas {
 			if(digito==digitoN(numero,i)) {
 				return i;
 			}
-			return -1;
 		}
 		return -1;
+	}
+/**
+ * quita una cantidad de cifras pasadas por parámetro
+ * al número pasado	
+ * @param numero int
+ * @param nCifras int
+ * @return int
+ */
+	public static int quitarPorDetras(int numero, int nCifras) {
+		for(int i=0;i<nCifras;i++) {
+			numero = numero/10;
+		}
+		return numero;
+	}
+/**
+ * quita el número de cifras pasada por parámetro a un 
+ * valor entero pasado	
+ * @param numero int
+ * @param nCifras int
+ * @return int
+ */
+	public static int quitarPorDelante(int numero, int nCifras) {
+		int longitud = digitos(numero);
+		int posicionCorte = longitud - nCifras;//obtiene la posición desde donde se hace el corte
+		int divisor = 10;
+		
+		if(posicionCorte>0) {
+			for(int i=0;i<posicionCorte;i++) {//se multiplica por 10 tantas veces como cifras se quieren quitar
+				divisor = divisor*10;
+			}
+			return numero%divisor;
+		}
+		return -1;
+	}
+/**
+ * concatena dos números enteros por detras	
+ * @param numeroA int
+ * @param numeroB int
+ * @return int
+ */
+	public static int pegarPorDetras(int numeroA,int numeroB) {
+		return Integer.valueOf(String.valueOf(numeroA) + String.valueOf(numeroB));
+	}
+/**
+ * concatena dos números enteros por delante
+ * @param numeroA int
+ * @param numeroB int
+ * @return int
+ */	
+	public static int pegarPorDelante(int numeroA,int numeroB) {
+		return Integer.valueOf(String.valueOf(numeroB) + String.valueOf(numeroA));
+	}
+/**
+ * devuelve perte de un númeropasado por parémetro
+ * desde uno a otro indice pasado	
+ * @param numero int
+ * @param posicionInicio int
+ * @param posicionFinal int
+ * @return int
+ */
+	public static int trozoDeNumero(int numero,int posicionInicio,int posicionFinal) {
+		int numeroResp;
+		int longitud;
+		
+		longitud = digitos(numero);
+		numero = quitarPorDetras(numero,(longitud-posicionFinal));
+		numero = quitarPorDelante(numero,posicionInicio);
+		return numero;
 	}
 	
 }
