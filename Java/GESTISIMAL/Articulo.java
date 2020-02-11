0299695950f0c3f4456ee04585af2c4fa356ff1f
albertommj <albertommj@gmail.com>
2020-02-11 18:08:13 +0100

    Create Articulo.java

---
+++ Java/GESTISIMAL/Articulo.java
@@ -0,0 +1,63 @@
+package gestisimal;
+import recursos.*;
+
+class Articulo {
+	private static int codigo;
+	private String descripcion;
+	private double precioCompra;
+	private double precioVenta;
+	private int stock;
+	
+	Articulo(String descripcion,double precioCompra,double precioVenta,int stock) {
+		int codigo = this.getCodigo();
+		this.setCodigo(codigo++);
+		this.setPrecioCompra(precioCompra);
+		this.setPrecioVenta(precioVenta);
+		this.setStock(stock);
+	}
+	
+	
+	//---Getters & Setters
+	public static int getCodigo() {
+		return codigo;
+	}
+	public static void setCodigo(int codigo) {
+		Articulo.codigo = codigo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecioCompra() {
+		return precioCompra;
+	}
+	public void setPrecioCompra(double precioCompra) {
+		this.precioCompra = precioCompra;
+	}
+	public double getPrecioVenta() {
+		return precioVenta;
+	}
+	public void setPrecioVenta(double precioVenta) {
+		if(this.precioCompra<this.precioVenta)
+			this.precioVenta = precioVenta;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		if(this.stock>0)
+			this.stock = stock;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Articulo [descripcion=" + descripcion + ", precioCompra=" + precioCompra + ", precioVenta="
+				+ precioVenta + ", stock=" + stock + "]";
+	}
+	
+	
+	
+}
