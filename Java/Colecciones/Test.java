9d2674dc6981b42de8ba0c2db4afeb3623e1c6ab
albertommj <albertommj@gmail.com>
2020-02-19 11:07:29 +0100

    Create Test.java

---
+++ Java/Colecciones/Test.java
@@ -0,0 +1,14 @@
+package ejercicio2;
+
+class Test {
+
+	public static void main(String[] args) {
+		Ej2 arrayList = new Ej2();
+		System.out.println("Suma: "+arrayList.getSuma());
+		System.out.println("Media: "+arrayList.getMedia());
+		System.out.println("Máximo: "+arrayList.getMaximo());
+		System.out.println("Mínimo: "+arrayList.getMinimo());
+
+	}
+
+}
