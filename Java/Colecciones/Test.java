df972602abfd8c84e634376d6a8a1c017f615495
albertommj <albertommj@gmail.com>
2020-02-22 10:42:57 +0100

    Add files via upload

--- Java/Colecciones/Test.java
+++ Java/Colecciones/Test.java
@@ -1,13 +1,9 @@
-package ejercicio2;
+package ejercicios;
 
 class Test {
 
 	public static void main(String[] args) {
-		Ej2 arrayList = new Ej2();
-		System.out.println("Suma: "+arrayList.getSuma());
-		System.out.println("Media: "+arrayList.getMedia());
-		System.out.println("Máximo: "+arrayList.getMaximo());
-		System.out.println("Mínimo: "+arrayList.getMinimo());
+		// TODO Auto-generated method stub
 
 	}
 
