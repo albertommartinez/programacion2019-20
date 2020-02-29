5fec70cb6823b44f505370a20f77f5f95b604919
albertommj <albertommj@gmail.com>
2020-02-29 13:03:08 +0100

    Create Test.java

---
+++ Java/Colecciones/Ej3/Test.java
@@ -0,0 +1,13 @@
+package ej3;
+
+class Test {
+
+	public static void main(String[] args) {
+		Ej3 array = new Ej3();
+		
+		array.introducirValores();
+		array.ordenarValores();
+		array.mostrarArrayList();
+	}
+
+}
