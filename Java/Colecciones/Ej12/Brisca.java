217f5526e86946b6765c7997bf831e3d0d2a85f0
albertommj <albertommj@gmail.com>
2020-03-02 22:16:19 +0100

    Add files via upload

---
+++ Java/Colecciones/Ej12/Brisca.java
@@ -0,0 +1,21 @@
+package ej12;
+
+import ej8.Baraja;
+
+class Brisca {
+	int puntuacion = 0; 
+	Baraja baraja = new Baraja();
+	
+	public static void main(String[] args) {
+		System.out.println("###############");
+		System.out.println("#  LA BRISCA  #");
+		System.out.println("###############");
+		String respuesta = recursos.Teclado.leerCadena("¿Quieres jugar?(s/n): ");
+		if(respuesta == "s") {
+			
+		}
+	}
+	
+	
+
+}
