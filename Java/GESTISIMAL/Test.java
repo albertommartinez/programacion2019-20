666c1662918aecca0b086c72c64c13a22f302304
albertommj <albertommj@gmail.com>
2020-04-09 13:14:52 +0200

    Add files via upload

--- Java/GESTISIMAL/Test.java
+++ Java/GESTISIMAL/Test.java
@@ -14,37 +14,61 @@ public class Test  {
 		double precioCompra = 0;
 		double precioVenta = 0;
 		int stock = 0;
-		String[] opciones = {"Añadir Articulo","Eliminar Articulo","Incrementar Articulo","Decrementar Articulo","Mostrar Articulos"};
+		int codigo;
+		boolean salir = false;
 		
-		//try {
-			do {
-				opcion = Menu.crearMenu("GESTISIMAL", opciones);
-				switch(opcion) {
-					case 1:
-						descripcion = Teclado.leerCadena("Introduce descripción :");
-						precioCompra = Teclado.leerDecimal("Introduce el precio de compra :");
-						precioVenta = Teclado.leerDecimal("Introduce el precio de venta :");
-						stock = Teclado.leerEntero("Introduce el stock disponible :");
-						almacen.annadirArticulo(descripcion, precioCompra, precioVenta, stock);
-						almacen.mostrarArticulo(almacen.almacen.size()-1);//muestra el ultimo articulo añadido(longitud almacen-1)
-						break;
-					case 2:
-						
-						almacen.listarArticulos();
-						opcion = Teclado.leerEntero("Introduce indice del articulo a borrar: ");
-						
-					case 3:
-						
-						int incremento = Teclado.leerEntero("Introduce cantidad a incrementar: ");
-						
-					case 5:
-						//almacen.toString();
-						almacen.listarArticulos();
-				}
-			}while(opcion!=0);
-		//}catch(ExcepcionEntradaError e) {
-			//System.out.println(e.getMessage());
-		//}
+		String[] opciones = {"Añadir Articulo","Eliminar Articulo","Incrementar Articulo","Decrementar Articulo","Mostrar Articulos","Salir"};
+				
+					do {
+						try {
+							opcion = Menu.crearMenu("GESTISIMAL", opciones);
+							switch(opcion) {
+								case 1:
+									descripcion = Teclado.leerCadena("Introduce descripción :");
+									precioCompra = Teclado.leerDecimal("Introduce el precio de compra :");
+									precioVenta = Teclado.leerDecimal("Introduce el precio de venta :");
+									stock = Teclado.leerEntero("Introduce el stock disponible :");
+									almacen.annadirArticulo(descripcion, precioCompra, precioVenta, stock);
+									almacen.mostrarArticulo(almacen.almacen.size()-1);//muestra el ultimo articulo añadido(longitud almacen-1)
+									break;
+								case 2:
+									
+									if(almacen.listarArticulos()==0) {
+										opcion = Teclado.leerEntero("Introduce código del articulo a borrar: ");
+										almacen.borrarArticulo(opcion);
+									}else{
+										System.out.println("No hay articulos en el almacen.");
+									}
+									break;
+								case 3:
+									codigo = Teclado.leerEntero("Introduce código de artículo: ");
+									int incremento = Teclado.leerEntero("Introduce cantidad a incrementar: ");
+									almacen.incrementarProducto(codigo, incremento);
+									break;
+								case 4:
+									codigo = Teclado.leerEntero("Introduce código de artículo: ");
+									int decremento = Teclado.leerEntero("Introduce cantidad a decrementar: ");
+									almacen.decrementarProducto(codigo, decremento);
+									break;
+								case 5:
+									//almacen.listarArticulos();
+									if(almacen.listarArticulos()!=0) {
+										System.out.println("--------------------------------");
+										System.out.println(" No hay productos en el almacen ");
+										System.out.println("--------------------------------");
+									}
+									break;
+								case 6:
+									salir = true;
+									break;
+								
+							}
+						}catch(Exception ExcepcionEntradaError) {
+							throw new ExcepcionEntradaError("Error en entrada de datos.Solo valores entre 1 y 6");
+							
+						} 
+					}while((!salir)||((!(opcion>0)&&(opcion<7))));
+		
 	}
 
 }
